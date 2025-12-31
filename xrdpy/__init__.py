a5e96a3e56d0e1264b26ad65590156612f7a7def
Badal Mondal <badal.mondal@tyndall.ie>
2025-12-31 15:27:44 +0000

    Added try except versioning in init

--- xrdpy/__init__.py
+++ xrdpy/__init__.py
@@ -1,4 +1,8 @@
-from ._version import version as __version__
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "d0.0.0"
+
 from .XRD import general_fns, xrd, plottings
 
 __all__ = ['general_fns', 'xrd', 'plottings']
